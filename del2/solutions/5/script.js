8a1e20575693d930e72373ef72ebe4331621f9dd
Mats Krüger Svensson <mats@matsemann.com>
2016-06-14 19:13:33 +0200

    del2 direkte SAP med pushstate

--- del2/solutions/5/script.js
+++ del2/solutions/5/script.js
@@ -11,9 +11,7 @@ function renderImages(data) {
     var img = data[i];
     html += `
       <figure>
-        <a href="${window.location.pathname}/${i}">
-          <img src="${img.url}" />
-        </a>
+        <img src="${img.url}" />
         <figcaption>${img.title}</figcaption>
       </figure>
     `;
@@ -23,22 +21,26 @@ function renderImages(data) {
 }
 
 function router() {
-  if (window.location.pathname === '/') {
-    return;
-  }
+  var tag = window.location.pathname;
 
-  var url = window.location.pathname.split('/');
+  if (tag == "/") { return; }
 
-  getPhotos(url[1]).then(function(data) {
+  getPhotos(tag).then(function(data) {
     var html = renderImages(data);
     document.querySelector('main').innerHTML = html;
   });
 }
 
-router();
-
 document.querySelector('form').addEventListener('submit', function(event) {
   event.preventDefault();
   var tag = event.target.querySelector('input').value;
-  window.location.pathname = '/' + tag;
+  history.pushState(null, '', tag);
+  router();
 });
+
+
+window.addEventListener('popstate', router);
+
+router();
+
+
