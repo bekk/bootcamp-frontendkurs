19469042fc6051cf7291f428329b68ed6f87c951
ingvild <ingvildindr@gmail.com>
2016-08-02 20:43:14 +0200

    Legg til UU-slides og oppgave

---
+++ del1/solutions/7/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var buttonsContainer = document.querySelector('.buttons');
+var output = document.querySelector('#output');
+
+buttonsContainer.addEventListener('click', function(event) {
+  var element = event.target;
+  if(element.nodeName !== 'BUTTON') { return; }
+
+  var number = element.textContent;
+  var originalNumber = output.textContent;
+
+  output.textContent = originalNumber === '0' ? number : originalNumber + number;
+});
